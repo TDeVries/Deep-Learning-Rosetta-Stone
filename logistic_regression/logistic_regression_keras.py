ef6163fb42df05c24c72e46cefd1bcf5b35868b2
TDeVries <randomtetra@gmail.com>
2017-02-10 12:49:24 -0500

    Update variable names to be consistent

--- logistic_regression/logistic_regression_keras.py
+++ logistic_regression/logistic_regression_keras.py
@@ -3,6 +3,7 @@ A logistic regression learning algorithm example using the Keras library.
 Author: Terrance DeVries
 Project: https://github.com/TDeVries/Deep-Learning-Rosetta-Stone
 '''
+
 from __future__ import print_function
 
 import numpy as np
@@ -16,9 +17,10 @@ from keras.utils import np_utils
 ### Parameters ###
 ##################
 learning_rate = 0.01
-nb_epoch = 10
+n_epoch = 10
 batch_size = 32
-nb_classes = 10
+n_input = 784
+n_classes = 10
 
 ######################
 ### Create dataset ###
@@ -26,21 +28,21 @@ nb_classes = 10
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
 # Flatten from 2D matrices to vectors
-train_X = train_X.reshape(60000, 784)
-test_X = test_X.reshape(10000, 784)
+train_X = train_X.reshape(len(train_X), n_input)
+test_X = test_X.reshape(len(test_X), n_input)
 
 # Scale values to be from 0 to 1 and convert to float
 train_X, test_X = train_X/255., test_X/255.
 
 # One-hot encode class labels
-train_Y = np_utils.to_categorical(train_y, nb_classes)
-test_Y = np_utils.to_categorical(test_y, nb_classes)
+train_Y = np_utils.to_categorical(train_y, n_classes)
+test_Y = np_utils.to_categorical(test_y, n_classes)
 
 #######################
 ### Construct model ###
 #######################
 model = Sequential()
-model.add(Dense(10, activation='softmax', input_shape=(784,)))
+model.add(Dense(10, activation='softmax', input_shape=(n_input,)))
 
 sgd = SGD(lr=learning_rate)
 model.compile(loss='mse', optimizer=sgd, metrics=['accuracy'])
@@ -48,7 +50,7 @@ model.compile(loss='mse', optimizer=sgd, metrics=['accuracy'])
 ###################
 ### Train model ###
 ###################
-model.fit(train_X, train_Y, batch_size=batch_size, nb_epoch=nb_epoch)
+model.fit(train_X, train_Y, batch_size=batch_size, nb_epoch=n_epoch)
 
 ######################
 ### Evaluate model ###
